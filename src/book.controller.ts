238c2dab16c9f85f76668e065fded18a81e31c05
Gaëtan Maisse <gmaisse@solocal.com>
2020-09-06 16:46:24 +0200

    feat: create API to manage books

--- src/book.controller.ts
+++ src/book.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { BookService } from './book.service';
+import { Book } from './Book';
 
-@Controller()
+@Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Get()
+  getBooks(@Query('author') author: string): Book[] {
+    if (author) {
+      return this.bookService.getBooksOf(author);
+    }
+    return this.bookService.getAllBooks();
+  }
+
+  @Post()
+  createBook(@Body() newBook: Book): Book {
+    this.bookService.addBook(newBook);
+
+    return this.bookService.getBook(newBook.title);
+  }
+
+  @Get('/:title')
+  getBook(@Param('title') title): Book {
+    return this.bookService.getBook(title);
+  }
+
+  @Delete('/:title')
+  deleteBook(@Param('title') title): void {
+    this.bookService.deleteBook(title);
+  }
 }
--- src/book.service.ts
+++ src/book.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from './Book';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  private readonly storage: Map<string, Book> = new Map();
+
+  addBook(book: Book): void {
+    this.storage.set(book.title, book);
+  }
+
+  getBook(name: string): Book | undefined {
+    return this.storage.get(name);
+  }
+
+  deleteBook(name: string): boolean {
+    return this.storage.delete(name);
+  }
+
+  getBooksOf(author: string): Book[] {
+    return Array.from(this.storage.values()).filter(
+      book => book.author === author,
+    );
+  }
+
+  getAllBooks() {
+    return Array.from(this.storage.values()).sort((bookA, bookB) =>
+      bookA.title.localeCompare(bookB.title),
+    );
+  }
+
+  getTotalNumberOfBooks() {
+    return this.storage.size;
+  }
+}
